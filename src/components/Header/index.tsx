e63078e6cd183bea07626f45f398ad47f217102a
marrcelosantana <marcelosantana@alu.ufc.br>
2022-03-28 23:39:04 -0300

    finish

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ export function Header () {
   return (
     <Container>
       <span>IMC</span>
-      <a href="https://github.com/marrcelosantana" target="_blank">
+      <a href="https://github.com/marrcelosantana" target="_blank" rel='noreferrer'>
         <AiFillGithub size={40} cursor='pointer' color='#ffffff'/>
       </a>
     </Container>
